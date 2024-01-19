6be08901d4857de7b0c6c554748ce1dcdbce5810
Jop Zitman <jop-zitman@hotmail.com>
2024-01-19 07:56:40 +0700

    update bert

--- bert/main.py
+++ bert/main.py
@@ -1,15 +1,32 @@
+import gc
+
+import numpy as np
 import tensorflow as tf
 from tqdm import tqdm
 from transformers import TFBertForSequenceClassification, BertTokenizer
+from keras.callbacks import CSVLogger
+from matplotlib import pyplot as plt
 
 from data.load.load import load_csv
+
+gpus = tf.config.list_physical_devices('GPU')
+if gpus:
+    # Restrict TensorFlow to only allocate 1GB of memory on the first GPU
+    try:
+        tf.config.set_logical_device_configuration(
+            gpus[0],
+            [tf.config.LogicalDeviceConfiguration(memory_limit=4000)])
+        logical_gpus = tf.config.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    except RuntimeError as e:
+        # Virtual devices must be set before GPUs have been initialized
+        print(e)
 
 tokenizer = BertTokenizer.from_pretrained('bert-base-multilingual-uncased')
 
 
 def encode(urls):
-    input_ids = []
-    attention_masks = []
+    input_ids, attention_masks = [], []
 
     for url in tqdm(urls):
         inputs = tokenizer.encode_plus(
@@ -21,39 +38,82 @@ def encode(urls):
         input_ids.append(inputs['input_ids'])
         attention_masks.append(inputs['attention_mask'])
 
-    return tf.concat(input_ids, 0), tf.concat(attention_masks, 0)
+    print("Encoding complete!")
+    print("Concatenating input_ids...")
+    concatenated_input_ids = tf.concat(input_ids, 0)
+    del input_ids
+    gc.collect()
+
+    print("Concatenating attention_masks...")
+    concatenated_attention_masks = tf.concat(attention_masks, 0)
+    del attention_masks
+    gc.collect()
+
+    return concatenated_input_ids, concatenated_attention_masks
 
 
 def main():
-    train_x, train_y, val_x, val_y, test_x, test_y = load_csv()
+    train_x, train_y, val_x, val_y, _, _ = load_csv()
+
+    train_samples = np.random.choice(len(train_x), size=100000, replace=False)
+    train_x = train_x[train_samples]
+    train_y = train_y[train_samples]
+
+    val_samples = np.random.choice(len(train_x), size=10000, replace=False)
+    val_x = val_x[val_samples]
+    val_y = val_y[val_samples]
 
     input_ids_train, attention_masks_train = encode(train_x)
     input_ids_val, attention_masks_val = encode(val_x)
-    input_ids_test, attention_masks_test = encode(test_x)
 
     labels_train = tf.convert_to_tensor(train_y)
     labels_val = tf.convert_to_tensor(val_y)
-    labels_test = tf.convert_to_tensor(test_y)
 
-    model = TFBertForSequenceClassification.from_pretrained('bert-base-multilingual-uncased')
-    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=2e-5),
+    model: tf.keras.Model = TFBertForSequenceClassification.from_pretrained('bert-base-multilingual-uncased')
+    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-5),
                   loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
                   metrics="accuracy")
 
-    batch_size = 6
-    epochs = 3
+    model.summary()
+
+    log_path = "logs.csv"
+
+    import pandas as pd
+
+    try:
+        history_df = pd.read_csv(log_path)
+        print("Model history loaded successfully!")
+    except Exception as e:
+        print("No model history found or error in loading. Building a new history.")
+        print("Error:", e)
+        # create empty df
+        history_df = pd.DataFrame()
 
     model.fit(
         [input_ids_train, attention_masks_train],
         labels_train,
         validation_data=([input_ids_val, attention_masks_val], labels_val),
-        batch_size=batch_size,
-        epochs=epochs
+        callbacks=[
+            CSVLogger(log_path, append=True),
+        ],
+        initial_epoch=history_df.shape[0],
+        epochs=4,
+        batch_size=6,
     )
 
-    result = model.evaluate([input_ids_test, attention_masks_test], labels_test)
-    print(f"Test Loss: {result[0]}")
-    print(f"Test Accuracy: {result[1]}")
+    # reload history after training
+    history_df = pd.read_csv(log_path)
+
+    metrics = ["accuracy", "loss", "precision", "recall"]
+    for metric in metrics:
+        plt.plot(history_df[metric])
+        plt.plot(history_df["val_" + metric])
+        plt.title("Model " + metric)
+        plt.ylabel(metric)
+        plt.xlabel("Epoch")
+        plt.legend(["Train", "Val"], loc="upper left")
+        plt.show()
+        plt.close()
 
 
 if __name__ == "__main__":
